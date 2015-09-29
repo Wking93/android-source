eaf7a3ec8de7349a64968d28a7fcdd11e35dad84
Will King <willking.king568@gmail.com>
2015-09-29 16:55:43 -0500

    checkpoint 14 complete

--- foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
+++ foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
@@ -15,22 +15,24 @@ public class WorstPractices extends Object {
 	int MAGIC_NUMBER = WorstPractices.animals(false);
 	MAGIC_NUMBER *= 5;
 	if (MAGIC_NUMBER > 18) {
-	while(MAGIC_NUMBER > 0)
-	{
+	while(MAGIC_NUMBER > 0)  {
 	MAGIC_NUMBER--;
-	}
-	}
+
+			}
+		}
 	}
 
+
 // animals
-// this method takes in a single parameter, yeswellheresthething. Using a very elaborate and complex algorithm, it calculate a magic number
-// yeswellheresthething: a seed which helps generate the magic number
-// returns: a magical number
+
+/*this method takes in a single parameter, yeswellheresthething. Using a very elaborate and complex algorithm, it calculate a magic number
+ yeswellheresthething: a seed which helps generate the magic number
+ returns: a magical number*/
+
 private static int animals(boolean yeswellheresthething)
 {
-		/*
-			Start off with one of these
-		*/
+		//Start off with one of these
+	
 	int aInt = yeswellheresthething ? 34 : 21;
 	float sparklesfairy = .5f;
 	for (int brown = 0; brown < aInt; brown++) { sparklesfairy *= aInt;
